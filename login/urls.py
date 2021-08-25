164f5d5eeb437fbf8d6049d06c4d4b105c7fc1eb
Juan Muñoz <cl.jmunoz@gmail.com>
2021-08-25 13:36:18 -0400

    Modificacion de templates de login, recovery password

--- login/urls.py
+++ login/urls.py
@@ -5,7 +5,8 @@ from login import views
 
 urlpatterns = [
     # path('dashboard/', include('dashboard.urls')),
-    path('',views.login, name='login')
-
-    
+    path('',views.login, name='login'),
+    path('forgotPassword',views.forgotPassword, name='forgotPassword'),
+    path('lockscreen',views.lockscreen, name='lockscreen'),
+    path('recoverPassword',views.recoverPassword, name='recoverPassword'),
 ]
--- login/views.py
+++ login/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def login(request):
     return render(request,'login/login.html')
+
+def forgotPassword(request):
+    return render(request,'login/forgot-password.html')
+
+def lockscreen(request):
+    return render(request,'login/lockscreen.html')
+
+def recoverPassword(request):
+    return render(request,'login/recover-password.html')
