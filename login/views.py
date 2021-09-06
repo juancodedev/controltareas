a1b1cde2b5a256575a9eb8b332f8e4a753ee6cdd
Juan Muñoz <cl.jmunoz@gmail.com>
2021-09-06 18:09:12 -0300

    Lipieza de codigo y se crea metodo que cierra sesion eliminando cookie

--- login/views.py
+++ login/views.py
@@ -12,12 +12,7 @@ from django.views.decorators.csrf import csrf_protect
 from django.contrib.auth import login,logout,authenticate
 from django.http import HttpResponseRedirect
 from django.contrib.auth.forms import AuthenticationForm
-from rest_framework.authtoken.models import Token
-from rest_framework.views import APIView
-from rest_framework import status
-from rest_framework.response import Response
 import requests, jwt, json
-from django.conf import settings
 
 def decodered(request):
     data = request.COOKIES.get('validate')
@@ -62,44 +57,22 @@ def validate(request):
     
 def Login(request):
     return render(request,'login/login.html')
-
-# class Login(FormView):
-#     template_name = "login/login.html"
-#     form_class = AuthenticationForm
-#     success_url = reverse_lazy('dashboard')
-
-#     @method_decorator(csrf_protect)
-#     @method_decorator(never_cache)
-#     def dispatch(self,request,*args,**kwargs):
-#         if request.user.is_authenticated:
-#             return HttpResponseRedirect(self.get_success_url())
-#         else:
-#             return super(Login,self).dispatch(request,*args,*kwargs)
-
-#     def form_valid(self,form):
-#         user = authenticate(username = form.cleaned_data['username'], password = form.cleaned_data['password'])
-#         token,_ = Token.objects.get_or_create(user = user)
-#         if token:
-#             login(self.request, form.get_user())
-#             return super(Login,self).form_valid(form)
-        
-#     def post(self,request,*args,**kwargs):
-#         return Response({'error'}, status=status.HTTP_400_BAD_REQUEST)
-
-
-
-# class Logout(APIView):
-#     def get(self,request, format = None):
-#         request.user.auth_token.delete()
-#         logout(request)
-#         return Response(status = status.HTTP_200_OK)
     
 def logout(request):
-    rep = redirect("/login/")
-    rep.delete_cookie('validate') # elimina el valor de la cookie de usuario establecido previamente en el navegador del usuario
-    return rep
+    if authenticated(request):
+        cookie = decodered(request)
+        token = cookie['data']['token']
+        rol = cookie['data']['rol']
+        if rol == 1:
+            headers={'Content-Type':'application/json', 'Authorization':'Token '+token}
+            data = requests.get('https://apitasktest.herokuapp.com/logout/', headers=headers)
+            rep = redirect('login')
+            rep.delete_cookie('validate') # elimina el valor de la cookie de usuario establecido previamente en el navegador del usuario
+            return rep
+        else:
+            return redirect('dashboard')
+
+    else:
+        return redirect('login')
 
 
-# import requests
-# headers={'Content-Type':'application/json', 'Authorization':'Token d070b44498fd12728d1e1cfbc9aa5f195600d21e'}
-# r = requests.get('http://localhost:8000/api/subscribers/', headers=headers)
