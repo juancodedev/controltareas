5996185a6cf622c027f8233f046b88bec9c58d80
Juan Muñoz <cl.jmunoz@gmail.com>
2021-11-16 19:29:51 -0300

    se elimina contraseña desde EMAIL_HOST_PASSWORD = '' en settings

--- controltareas/settings.py
+++ controltareas/settings.py
@@ -154,5 +154,5 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
 EMAIL_HOST_USER = 'controltareasnotificaciones@gmail.com'
-EMAIL_HOST_PASSWORD = 'olidata123'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
