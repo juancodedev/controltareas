00d49ffcf42e45cdafbfae121e3cf67888a7c1ba
Juan Muñoz <cl.jmunoz@gmail.com>
2021-11-17 03:12:51 -0300

    actualización de contraseña
    actualización de contraseña

--- controltareas/settings.py
+++ controltareas/settings.py
@@ -147,6 +147,6 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'controltareasnotificaciones@gmail.com'
-EMAIL_HOST_PASSWORD = 'olidata123'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 DEFAULT_FROM_EMAIL = 'controltareasnotificaciones@gmail.com'
